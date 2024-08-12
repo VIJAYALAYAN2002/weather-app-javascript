e9206a129b06811d0b5efac8f9b3c7f269ea8fbc
VIJAYALAYAN K <122135663+VIJAYALAYAN2002@users.noreply.github.com>
2024-08-12 10:04:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 // api key : 82005d27a116c2880c8f0fcb866998a0
 
-// SELECT ELEMENTS
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
 const notificationElement = document.querySelector(".notification");
 
-// App data
 const weather = {};
 
 weather.temperature = {
     unit : "celsius"
 }
 
-// APP CONSTS AND VARS
 const KELVIN = 273;
-// API KEY
 const key = "82005d27a116c2880c8f0fcb866998a0";
-
-// CHECK IF BROWSER SUPPORTS GEOLOCATION
 if('geolocation' in navigator){
     navigator.geolocation.getCurrentPosition(setPosition, showError);
 }else{
@@ -27,7 +21,6 @@ if('geolocation' in navigator){
     notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
 }
 
-// SET USER'S POSITION
 function setPosition(position){
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
@@ -35,7 +28,6 @@ function setPosition(position){
     getWeather(latitude, longitude);
 }
 
-// SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
 function showError(error){
     notificationElement.style.display = "block";
     notificationElement.innerHTML = `<p> ${error.message} </p>`;
